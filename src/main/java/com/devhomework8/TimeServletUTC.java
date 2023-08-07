63b0a665e57db73d77515495ac7a6f8f095d01dc
khylyk <85830910+khylyk@users.noreply.github.com>
2023-08-07 15:31:46 +0200

    commit devhomework 8 - 3

--- src/main/java/com/devhomework8/TimeServletUTC.java
+++ src/main/java/com/devhomework8/TimeServletUTC.java
@@ -1,67 +1,40 @@
-package com.devhomework8;//package com.devhomework8;
-//
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.time.LocalDateTime;
-//import java.time.ZoneId;
-//import java.time.ZonedDateTime;
-//import java.time.format.DateTimeFormatter;
-//
-//@WebServlet(value = "/time")
-//public class TimeServletUTC extends HttpServlet {
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        resp.setContentType("text/html; charset=utf-8");
-//
-//        String timeZoneParam = req.getParameter("timezone");
-//        ZoneId zoneId;
-//
-//        if (timeZoneParam != null && !timeZoneParam.isEmpty()) {
-//            try {
-//                zoneId = ZoneId.of(timeZoneParam);
-//            } catch (Exception e) {
-//                resp.getWriter().write("error with time zone parameter");
-//                return;
-//            }
-//        } else {
-//            zoneId = ZoneId.of("UTC");
-//        }
-//
-//        LocalDateTime localDateTime = LocalDateTime.now();
-//        ZonedDateTime zonedDateTime = localDateTime.atZone(ZoneId.systemDefault()).withZoneSameInstant(zoneId);
-//        String formattedTime = zonedDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss 'UTC'xxx"));
-//
-//        resp.getWriter().write(formattedTime);
-//    }
-//}
+package com.devhomework8;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
-@WebServlet("/time")
+@WebServlet(value = "/time")
 public class TimeServletUTC extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        response.setCharacterEncoding("UTF-8");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf-8");
 
-        String timezoneParam = request.getParameter("timezone");
-        TimeZone timezone = TimeZone.getTimeZone(timezoneParam != null ? timezoneParam : "UTC");
+        String timeZoneParam = req.getParameter("timezone");
+        ZoneId zoneId;
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
-        dateFormat.setTimeZone(timezone);
-        String currentTime = dateFormat.format(new Date());
+        if (timeZoneParam != null && !timeZoneParam.isEmpty()) {
+            try {
+                zoneId = ZoneId.of(timeZoneParam);
+            } catch (Exception e) {
+                resp.getWriter().write("error with time zone parameter");
+                return;
+            }
+        } else {
+            zoneId = ZoneId.of("UTC");
+        }
 
-        String htmlResponse = "<html><body><h2>Current Time</h2><p>" + currentTime + "</p></body></html>";
-        response.getWriter().write(htmlResponse);
+        LocalDateTime localDateTime = LocalDateTime.now();
+        ZonedDateTime zonedDateTime = localDateTime.atZone(ZoneId.systemDefault()).withZoneSameInstant(zoneId);
+        String formattedTime = zonedDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss 'UTC'xxx"));
+
+        resp.getWriter().write(formattedTime);
     }
 }
+
